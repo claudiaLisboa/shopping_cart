bdc1222a53b121417696bdbb9e254c49c5e74194
Claudia Lisboa <ccalumby@gmail.com>
2023-01-04 21:15:35 -0500

    added NavLink

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,24 @@
-import { Navbar as NavBarBs} from "react-bootstrap"
+import { Container, Nav, Navbar as NavBarBs} from "react-bootstrap"
+import { NavLink } from "react-router-dom"
 
 
 export function Navbar(){
-    return<NavBarBs>NavBar</NavBarBs>
+    return(
+        <NavBarBs className="bg-white shadow-sm mb-3">
+            <Container> 
+                <Nav>
+                    <Nav.Link to="/" as={NavLink}>
+                            Home
+                    </Nav.Link>
+                    <Nav.Link to="/store" as={NavLink}>
+                            Store
+                    </Nav.Link>
+                    <Nav.Link to="/about" as={NavLink}>
+                            About
+                    </Nav.Link>
+                </Nav>
+
+            </Container>
+        </NavBarBs>
+    )
 }

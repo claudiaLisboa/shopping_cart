56c6b5480659a828d857a1a0e59f2ab6077fa14b
Claudia Lisboa <ccalumby@gmail.com>
2023-01-04 20:38:26 -0500

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-
+import { Routes, Route} from "react-router-dom"
+import { Container } from "react-bootstrap"
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <div className="App">
-      Dora LINDA !
-    </div>
+    <Container >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
   )
 }
 

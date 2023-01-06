bc603fa03b9416a48679f20346296c66361b29d0
Claudia Lisboa <ccalumby@gmail.com>
2023-01-05 21:29:21 -0500

    added unction getItemQuantity, increase decrise

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -3,10 +3,12 @@ import { createContext, ReactNode, useContext, useState } from "react";
 type ShoppingCartProviderProps = {
     children: ReactNode
 }
+
 type CartItem={
     id: number
     quantity: number
 }
+
 type ShoppingCartContext = {
     getItemQuantity: (id: number) => number
     increaseCartQuantity: (id: number) => void
@@ -23,7 +25,45 @@ export function useShoppingCart(){
 
 export function ShoppingCartProvider({ children }:ShoppingCartProviderProps){
     const [cartItems, setCartItems] = useState<CartItem[]>([])
-    
+
+
+    function getItemQuantity(id:number){
+        return cartItems.find(item => item.id === id)?.quantity || 0
+    }
+
+    function increaseCartQuantity(id: number) {
+        setCartItems(currItems => {
+            if(currItems.find(item => item.id === id) == null){
+                return [...currItems, {id, quantity:1 }]
+            } else{
+                    return currItems.map(item => {
+                    if(item.id === id ){
+                       return{...item, quantity:item.quantity +1}
+                    }else{
+                        return item
+                    }
+                   })
+            }
+        })
+    }
+
+    function decreaseCartQuantity(id: number) {
+        setCartItems(currItems => {
+            if(currItems.find(item => item.id === id)?.quantity === 1 ){
+                return currItems.filter(item => item.id !==id)
+            } else{
+                return currItems.map(item => {
+                    if(item.id === id ){
+                        return{...item, quantity:item.quantity +1}
+                    }else{
+                        return item
+                    }
+                })
+            }
+        })
+    }
+  
+
     return (
             <ShoppingCartContext.Provider value={{ }}>
                     {children}

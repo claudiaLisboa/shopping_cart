1cc4bca753eff3af533dccac6e43b14e8b970882
Claudia Lisboa <ccalumby@gmail.com>
2023-01-06 18:04:05 -0500

    updated unction ShoppingCart  Offcanvas.Body

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { CarouselItem, Offcanvas, OffcanvasHeader, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
 import { CartItem } from "./CartItem";
+import storeItems from "../data/items.json"
 
 type ShoppingCartProps ={
     isOpen:boolean
@@ -20,6 +22,14 @@ export function ShoppingCart({isOpen}:ShoppingCartProps){
                     {cartItems.map(item => (
                         <CartItem key={item.id} {...item}/> )
                    )}
+                   <div className="ms-auto fw-bold fs-5">
+                        Total{" "}
+                        {formatCurrency(
+                            cartItems.reduce((total, cartItem) => {
+                                const item = storeItems.find(i => i.id === cartItem.id)
+                                return total + (item?.price || 0 ) * cartItem.quantity}, 0)
+                          )} 
+                   </div>
                 </Stack>
             </Offcanvas.Body>
         </Offcanvas>

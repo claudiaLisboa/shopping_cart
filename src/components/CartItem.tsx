3d323c26621c2bcd4d234f7d91fa1da488497757
Claudia Lisboa <ccalumby@gmail.com>
2023-01-06 19:14:14 -0500

    fixed error function CartItem()

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -11,29 +11,42 @@ type CartItemProps ={
 
 export function CartItem({id, quantity}:CartItemProps){
 
-    const {removeFromCart} = useShoppingCart()
-    const item = storeItems.find(i => i.id === id)
+const {removeFromCart} = useShoppingCart()
+const item = storeItems.find(i => i.id === id)
 
-    if(item == null)  return null
+if(item == null)  return null
       
-    return(
-        <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
-            <img 
-               src={item.imgUrl} 
-               style={{ width: "125px" , height:"75px", objectFit:"cover"}}
-            />
-            <div className="me-auto">
-                <div>
-                    {item.name} {quantity >1 &&  <span className="text-muted" style={{ fontSize:".65rem"  }}>{quantity} x</span>}
-                </div>
-                <div className="text-muted" style={{ fontSize:".75rem" }}>{formatCurrency(item.price)}</div>
-                <div className="text-muted" style={{ fontSize:".75rem" }}>{formatCurrency(item.price * quantity)}</div>
+return (
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
+        <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+        />
+        <div className="me-auto">
+            <div>
+                {item.name}{" "}
+                {quantity > 1 && (
+                <span className="text-muted" style={{ fontSize: ".65rem" }}>
+                    x{quantity}
+                </span>
+                )}
             </div>
-            <Button 
-                variant="outline-danger" size="sm" 
-                onClick={() => removeFromCart(item.id)}>&times; </Button>
-        </Stack>
+            <div 
+                className="text-muted" 
+                style={{ fontSize: ".75rem" }}
+            >
+            {formatCurrency(item.price)}
+            </div>
+        </div>
+            <div> 
+                {formatCurrency(item.price * quantity)}
+            </div>
+            <Button
+                variant="outline-danger"
+                size="sm"
+                onClick={() => removeFromCart(item.id)}
+                > &times; 
+            </Button>
+    </Stack>
     )
-
-
 }

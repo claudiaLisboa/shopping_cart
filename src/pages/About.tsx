a0848d94aca0fb6e60e3b095c064e82b481a837b
Claudia Lisboa <ccalumby@gmail.com>
2023-01-31 15:03:05 -0500

    added footer

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,13 @@
 export function About(){
-    return <h1>About</h1>
+return(
+    <>
+    <h1>About</h1>
+        <div className="footer"  >
+            <h5> <span className="created-by">Created by Claudia Lisboa - For studying purpose</span> </h5> 
+            <div>© 2021 Copyright :
+            <a href="https://github.com/claudiaLisboa"> https://github.com/claudiaLisboa </a>
+            </div>
+        </div>
+    </>
+);
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,14 @@
+
 export  function Home(){
-    return <h1>HOME</h1>
+return ( 
+    <>
+    <h1>HOME</h1>
+        <div className="footer"  >
+              <h5> <span className="created-by">Created by Claudia Lisboa - For studying purpose</span> </h5> 
+            <div>© 2021 Copyright :
+            <a href="https://github.com/claudiaLisboa"> https://github.com/claudiaLisboa </a>
+            </div>
+        </div>
+    </>
+);
 }
